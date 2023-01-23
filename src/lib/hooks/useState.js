ecd7ce69a993cf6f54c634832340ae19c348a155
Vladislav <vladislav.palaev@yandex.ru>
2023-01-24 01:53:56 +0300

    fix(workloop): некорректно одновременно обходил массив и связанный список
    Была проблема в том, что когда у нас есть новые элементы на добавление в dom дерево, так как это
    новые элементы и у них нет старого волокна, в одни момент при обходе массива и связанного списка
    sibling становился Null и мы пытались вызвать у него sibling, поэтому вылетала ошибка

--- src/lib/hooks/useState.js
+++ src/lib/hooks/useState.js
@@ -1,42 +1,41 @@
 // для работы с fiber узлом, есть глобальная переменная React.workingFiber
 export function useState(initialState) {
-	const oldHook = React.workingFiber.alternate &&
-					React.workingFiber.alternate.hooks &&
-					React.workingFiber.alternate.hooks[React.hookIndex];
-	// хук по сути это какой-то изначальный стейт и очередь из каких-то действий
-	// по отношению к нему
-	const hook = {
-		state: oldHook ?
-				oldHook.state :
-				initialState instanceof Function ?
-				initialState() :
-				initialState,
-		queue: [],
-	};
-
-	const actions = oldHook ? oldHook.queue : [];
-	actions.forEach((action) => {
-		hook.state = action instanceof Function ? action(hook.state) : action;
-	})
-
-	const setState = (action) => {
-		hook.queue.push(action);
-
-		// Вызываем ререндер компонента, React начнет обход fiber tree
-		// recocle будет решать какой effectTag применять к node узлам
-		React.workingRoot = {
-			node: React.currentRoot.node,
-			props: React.currentRoot.props,
-			alternate: React.currentRoot
-		}
-		// Обнуляем некоторые browserGlobalVar для запуска ререндера и
-		// и узлов для удаления.
-		React.nextUnitOfWork = React.workingRoot;
-		React.nodesToRemove = [];
+    const oldHook = React.workingFiber.alternate &&
+        React.workingFiber.alternate.hooks &&
+        React.workingFiber.alternate.hooks[React.hookIndex];
+    // хук по сути это какой-то изначальный стейт и очередь из каких-то действий
+    // по отношению к нему
+    const hook = {
+        state: oldHook ?
+            oldHook.state :
+            initialState instanceof Function ?
+                initialState() :
+                initialState,
+        queue: [],
+    };
+    const actions = oldHook ? oldHook.queue : [];
+    actions.forEach((action) => {
+        hook.state = action instanceof Function ? action(hook.state) : action;
+    });
+    const setState = (action) => {
+        hook.queue.push(action);
+        // Вызываем ререндер компонента, React начнет обход fiber tree
+        // recocle будет решать какой effectTag применять к node узлам
+        React.workingRoot = {
+            node: React.currentRoot.node,
+            props: React.currentRoot.props,
+            alternate: React.currentRoot
+        };
+        // Обнуляем некоторые browserGlobalVar для запуска ререндера и
+        // и узлов для удаления.
+        React.nextUnitOfWork = React.workingRoot;
+        React.nodesToRemove = [];
+    };
+	// Добавил эти строчки
+	if (React.workingFiber && React.workingFiber.hooks) {
+		React.workingFiber.hooks.push(hook);
+		React.hookIndex++;
 	}
 
-	React.workingFiber.hooks.push(hook);
-	React.hookIndex += 1;
-
-	return [hook.state, setState]
+    return [hook.state, setState];
 }
--- src/lib/workLoop.js
+++ src/lib/workLoop.js
@@ -1,88 +1,76 @@
 import { isFunctionComponent } from "../utils/simpleHelpers.js";
 import { updateFunctionComponent, updateHostComponent, updateNode } from "./FiberEffectTag.js";
-
 export function workLoop(deadline) {
-	while (React.nextUnitOfWork && deadline.timeRemaining() >= 0) {
-		React.nextUnitOfWork = performUnitOfWork(React.nextUnitOfWork);
-	}
-
-	if (!React.nextUnitOfWork && React.workingRoot) {
-		requestAnimationFrame(commitRoot);
-	}
-
-	requestIdleCallback(workLoop);
+    while (React.nextUnitOfWork && deadline.timeRemaining() >= 0) {
+        React.nextUnitOfWork = performUnitOfWork(React.nextUnitOfWork);
+    }
+    if (!React.nextUnitOfWork && React.workingRoot) {
+        requestAnimationFrame(commitRoot);
+    }
+    requestIdleCallback(workLoop);
 }
-
 // выполняем работу по добавлению node узла в dom и возращаем next работу, если
 // если есть child, то его, если нет, то sibling, либо идем к parent
 function performUnitOfWork(fiber) {
-	if (isFunctionComponent(fiber)) {
-		updateFunctionComponent(fiber);
-	} else {
-		updateHostComponent(fiber);
-	}
-
-	if (fiber.child) {
-		return fiber.child;
-	}
-
-	let nextFiber = fiber;
-	// если нет children, то будем искать sibling, если нет и его
-	// то идем вверх к parent, до тех пор, пока nextFiber не станет undefined
-	while (nextFiber) {
-		if (nextFiber.sibling) {
-			return nextFiber.sibling;
-		}
-
-		nextFiber = nextFiber.parent;
-	}
+    if (isFunctionComponent(fiber)) {
+        updateFunctionComponent(fiber);
+    }
+    else {
+        updateHostComponent(fiber);
+    }
+    if (fiber.child) {
+        return fiber.child;
+    }
+    let nextFiber = fiber;
+    // если нет children, то будем искать sibling, если нет и его
+    // то идем вверх к parent, до тех пор, пока nextFiber не станет undefined
+    while (nextFiber) {
+        if (nextFiber.sibling) {
+            return nextFiber.sibling;
+        }
+        nextFiber = nextFiber.parent;
+    }
 }
-
 
 // Функция вызывается когда у нас больше нет работы по готовке fiber элементов
 // далее функция commitWork вставит все эти узлы взависимости от их отношений
 function commitRoot() {
-	React.nodesToRemove.forEach(commitWork);
-	commitWork(React.workingRoot.child);
-
-	React.currentRoot = React.workingRoot;
-	React.workingRoot = null;
+    React.nodesToRemove.forEach(commitWork);
+    commitWork(React.workingRoot.child);
+    React.currentRoot = React.workingRoot;
+    React.workingRoot = null;
 }
-
 // Рекурсивно комитим готовые наши узлы в друг друга
 // fiber.parent.appendChild(fiber.node);
 function commitWork(fiber) {
-	if (!fiber) {
-		return;
-	}
-
-	let parentFiber = fiber.parent;
-
-	// проверяем на компонент, так как у него нет node узла, а нам
-	// нужен node узел для вставки следующего узла
-	while (!parentFiber.node) {
-		parentFiber = parentFiber.parent;
-	}
-
-	const parentNode = parentFiber.node;
-
-	if (fiber.action === 'ADD' && fiber.node != null) {
-        parentNode.appendChild(fiber.node);
-    } else if (fiber.action === 'UPDATE' && fiber.node != null) {
-        updateNode(fiber.node, fiber.alternate.props, fiber.props);
-    } else if (fiber.action === 'REMOVE') {
-        commitRemove(fiber)  //todo подумать чтобы ниже было return
+    if (!fiber) {
+        return;
     }
-
-	commitWork(fiber.child);
-	commitWork(fiber.sibling);
+    let parentFiber = fiber.parent;
+    // проверяем на компонент, так как у него нет node узла, а нам
+    // нужен node узел для вставки следующего узла
+    while (!parentFiber.node) {
+        parentFiber = parentFiber.parent;
+    }
+    const parentNode = parentFiber.node;
+    if (fiber.action === 'ADD' && fiber.node != null && parentNode) {
+        parentNode.appendChild(fiber.node);
+    }
+    else if (fiber.action === 'UPDATE' && fiber.node != null && fiber.alternate) {
+        updateNode(fiber.node, fiber.alternate.props, fiber.props);
+    }
+    else if (fiber.action === 'REMOVE' && parentNode) {
+        commitRemove(parentNode, fiber); //todo подумать чтобы ниже было return
+		return;
+    }
+    commitWork(fiber.child);
+    commitWork(fiber.sibling);
 }
-
 // при удалении узла мы двигаемся вниз, пока не найден потомка с узлом.
-function commitRemove(fiber) {
-	if (fiber.node) {
-		return fiber.node.remove();
+function commitRemove(parentFiber, fiber) {
+    if (fiber && fiber.node) {
+		parentFiber.removeChild(fiber.node);
+	} else if (fiber && fiber.child) {
+		commitRemove(parentFiber, fiber.child);
 	}
-
-	commitRemove(fiber.child);
 }
